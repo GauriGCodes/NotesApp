292e1ced747f00a34226f36528c5c37e86d7b655
Gauri Gupta <126117971+GauriGCodes@users.noreply.github.com>
2023-11-04 04:55:34 +0000

    Update tasks.json and Notes.js

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,64 +1,75 @@
-import { useLayoutEffect } from 'react';
-import './css/Notes.css';
-import { useRef, useState } from 'react';
+import { useLayoutEffect } from "react";
+import "./css/Notes.css";
+import { useRef, useState } from "react";
 
 const MIN_TEXT_HEIGHT = 60;
 let ID = 0;
 
 function Notes() {
-    const textareaAref = useRef(null);
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [notes, setNotes] = useState([]);
+  const textareaAref = useRef(null);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [notes, setNotes] = useState([]);
 
-    const onChange = (e) => setContent(e.target.value);
-    const onSubmit = (e) => {
-        const Height = textareaAref.current.scrollHeight;
-        console.log(Height);
-        setNotes([
-            ...notes,
-            { title: title, content: content, index: ID++}
-        ]);
-        setTitle("");
-        setContent("");
-    }
+  const onChange = (e) => setContent(e.target.value);
+  const onSubmit = (e) => {
+    const Height = textareaAref.current.scrollHeight;
+    console.log(Height);
+    setNotes([...notes, { title: title, content: content, index: ID++ }]);
+    setTitle("");
+    setContent("");
+  };
 
-    useLayoutEffect(() => {
-        textareaAref.current.style.height = "inherit";
-        textareaAref.current.style.height = `${Math.max(
-            textareaAref.current.scrollHeight,
-            MIN_TEXT_HEIGHT
-        )}px`;
+  useLayoutEffect(() => {
+    textareaAref.current.style.height = "inherit";
+    textareaAref.current.style.height = `${Math.max(
+      textareaAref.current.scrollHeight,
+      MIN_TEXT_HEIGHT,
+    )}px`;
+  }, [content]);
 
-    }, [content]);
-
-    return (
-        <div>
-            <div className="Notes">
-                    <input type="text" name="title" className='content' placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
-                    <textarea onChange={onChange} ref={textareaAref} style={{
-                        minHeight: MIN_TEXT_HEIGHT,
-                        resize: "none"
-                    }} name="content" placeholder="Take a note..." value={content} wrap="soft" />
-                <button onClick={onSubmit}>Add</button>
-            </div>
-            <div className='Notes-Container'>
-                {notes.map((note) => (
-                    <div className='Note' key={note.index}>
-                        <p>{note.index}</p>
-                        <h3>{note.title}</h3>
-                        <p>{note.content}</p>
-                        <button onClick={() => {
-                            setNotes(
-                                notes.filter( n => n.index !== note.index)
-                            );
-                        }}>Delete</button>
-                    </div>
-
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="Notes">
+        <input
+          type="text"
+          name="title"
+          className="content"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+          onChange={onChange}
+          ref={textareaAref}
+          style={{
+            minHeight: MIN_TEXT_HEIGHT,
+            resize: "none",
+          }}
+          name="content"
+          placeholder="Take a note..."
+          value={content}
+          wrap="soft"
+        />
+        <button onClick={onSubmit}>Add</button>
+      </div>
+      <div className="Notes-Container">
+        {notes.map((note) => (
+          <div className="Note" key={note.index}>
+            <h3>{note.title}</h3>
+            <p>{note.content}</p>
+            <button
+              onClick={() => {
+                setNotes(notes.filter((n) => n.index !== note.index));
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Notes;
